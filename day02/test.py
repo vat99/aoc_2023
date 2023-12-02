41167b1045471f1312d81af67b166dc6d3018977
vat99 <talwar_varun@bah.com>
2023-12-02 14:54:07 -0500

    fix tests

--- day02/test.py
+++ day02/test.py
@@ -6,9 +6,9 @@ def solution():
     return Solution()
 
 parse_digit_cases = [
-    ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", (1, {"blue": 9, "red": 5, "green": 4})),
-    ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", (2, {"blue": 6, "red": 1, "green": 6})),
-    ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", (3, {"blue": 11, "red": 25, "green": 26}))
+    ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", (1, {"blue": 6, "red": 4, "green": 2})),
+    ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", (2, {"blue": 4, "red": 1, "green": 3})),
+    ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", (3, {"blue": 6, "red": 20, "green": 13}))
 ]
 
 @pytest.mark.parametrize(
